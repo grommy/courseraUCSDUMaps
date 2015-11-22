46f8a273c170b307a13a19701e548331f921174c
Nick <koteloks@yandex.ua>
2015-11-22 21:04:58 +0200

    week 2 completed (1 file forgot)

---
+++ src/module3/MagnitudeScale.java
@@ -0,0 +1,43 @@
+package module3;
+
+
+public enum MagnitudeScale {
+    Minor {
+        public int getColor() {
+            return -16776961;  //blue
+        }
+        public int getSize() {
+            return 5;
+        }
+        public String getDescription() {
+            return "Below 4.0";
+        }
+    },
+    Medium {
+        public int getColor() {
+            return -256;  //yellow
+        }
+        public int getSize() {
+            return 10;
+        }
+        public String getDescription() {
+            return "4.0+ magnitude";
+        }
+    },
+    Major {
+        public int getColor() {
+            return -65536;  //red
+        }
+        public int getSize() {
+            return 20;
+        }
+        public String getDescription() {
+            return "5.0+ magnitude";
+        }
+
+    };
+
+    public abstract int getColor();
+    public abstract int getSize();
+    public abstract String getDescription();
+}
